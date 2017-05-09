5f8d00ea31c109f9d36ee2fb6bf870accacf263c
Candido Souza <candido_souzza@hotmail.com>
2017-05-09 19:57:37 -0300

    Primeiro commit no repositório

---
+++ app01_hello_world/hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def hello():
+    print("Hello World!!!")
+
+hello()
---
+++ app02_game_alien_invasion/settings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+
+
+class Settings:
+    """
+    Uma classe para armazenar todas as configurações da Invasão Alienigena.
+    """
+
+    def __init__(self):
+        """
+        Inicia as configurações do jogo
+        """
+        # Configurações da tela
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
