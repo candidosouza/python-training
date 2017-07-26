dabebc986cf54fe211b54da1f9143cec00817294
Candido Souza <candido_souzza@hotmail.com>
2017-07-26 10:51:32 -0300

    Desempacotar em sequência

---
+++ livro_python_cookbook/01_estrutura_de_dados_e_algoritmos/1.1_desempacotar_em_sequencia.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+data = ['ACME', 77, 97.7, (2017,07,26)]
+
+name, shares, price, date = data
+
+print(name)
+print(shares)
+print(price)
+print(date)
+
+s = 'Hello'
+
+a, b, c, d, e = s
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# descartar sequencias
+
+c = 'Python'
+
+a, _, _, _, b, _ = c
+print(a)
+print(b)
+
